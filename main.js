96f960a8a10ffd1e4bf0a8cf7091a9ca0602d2c9
scamas322 <95655459+scamas322@users.noreply.github.com>
2022-11-30 15:23:23 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,95 @@
+song1 = "";
+song2 = "";
+
+song1status = "";
+song2status= "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+	song1 = loadSound("just-shapes-beats-lycanthropy-rank-s.mp3");
+    song2 = loadSound("yt1s.com - Just Shapes Beats Barracuda rank S.mp3");
+}
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
+}
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+    song1status = song1.isPlaying();
+    song2status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+        song2.stop();
+        if(song1status == false)
+        {
+            song1.play();
+            document.getElementById("nombre_cancion").innerHTML = "reproduciendo: lycanthropy";
+
+        }
+	}
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+        song1.stop();
+        if(song2status == false)
+        {
+            song2.play();
+            document.getElementById("nombre_cancion").innerHTML = "reproduciendo: barracuda";
+
+        }
+	}
+
+}
+
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
